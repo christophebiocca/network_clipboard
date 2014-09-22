9ea076785b1b67fb858614adeaec6b801bfbb96f
Christophe Biocca <christophe.biocca@gmail.com>
2014-09-21 23:57:17 -0400

    Establish 2-way encrypted connection to peers.

--- lib/network_clipboard/client.rb
+++ lib/network_clipboard/client.rb
@@ -2,17 +2,22 @@ require_relative 'config'
 require_relative 'discovery'
 require_relative 'connection'
 
+require 'socket'
+
 module NetworkClipboard
   class Client
     def initialize
       @config = Config.new
       @discovery = Discovery.new(@config)
+      @tcp_server = TCPServer.new(@config.port)
+      @connections = {}
     end
 
     def loop
       while true
         [:announce,
          :discover,
+         :find_incoming,
          :wait,
         ].each do |action|
           send(action)
@@ -26,12 +31,46 @@ module NetworkClipboard
 
     def discover
       @discovery.get_peer_announcements do |remote_client_id,address|
+        next if @connections[remote_client_id] or remote_client_id < @config.client_id
+
+        aes_connection = AESConnection.new(@config,TCPSocket.new(address,@config.port))
+
+        if aes_connection.remote_client_id != remote_client_id
+          aes_connection.close
+          next
+        end
+          
+        if @connections[aes_connection.remote_client_id]
+          aes_connection.close
+          next
+        end
+
+        @connections[aes_connection.remote_client_id] = aes_connection
+      end
+    end
+
+    def find_incoming
+      while true
+        begin
+          incoming = @tcp_server.accept_nonblock
+        rescue IO::WaitReadable
+          return
+        end
+        aes_connection = AESConnection.new(@config,incoming)
+
+        if @connections[aes_connection.remote_client_id]
+          aes_connection.close
+          next
+        end
+
+        @connections[aes_connection.remote_client_id] = aes_connection
       end
     end
 
     def wait
       return IO.select([
         @discovery.receive_socket,
+        @tcp_server,
       ], [], [], 5)
     end
   end
--- lib/network_clipboard/connection.rb
+++ lib/network_clipboard/connection.rb
@@ -1,4 +1,80 @@
+require 'socket'
+require 'openssl'
+
 module NetworkClipboard
+  HANDSHAKE_STRING = "NetworkClipboard Handshake"
+
+  class HandshakeException < Exception
+  end
+
   class AESConnection
+
+    attr_reader :remote_client_id
+
+    def initialize(config,socket)
+      @socket = socket
+
+      @encryptor = OpenSSL::Cipher::AES.new(128, :CBC)
+      @encryptor.encrypt
+      @encryptor.key = config.secret
+
+      @decryptor = OpenSSL::Cipher::AES.new(128, :CBC)
+      @decryptor.decrypt
+      @decryptor.key = config.secret
+
+      handshake(config.client_id)
+    end
+
+    def handshake(client_id)
+      iv = @encryptor.random_iv
+      @socket.send([iv.size].pack('N'),0)
+      @socket.send(iv,0)
+      iv_size = @socket.recv(4).unpack('N')[0]
+      @decryptor.iv = @socket.recv(iv_size)
+
+      # Verify it all worked.
+      send(HANDSHAKE_STRING)
+      raise HandshakeException unless receive == HANDSHAKE_STRING
+
+      send(client_id)
+      @remote_client_id = receive
+    end
+
+    def send(new_content)
+      ciphertext = @encryptor.update(new_content) + @encryptor.final
+      @socket.send([ciphertext.size].pack('N'),0)
+      @socket.send(ciphertext,0)
+      @encryptor.reset
+    end
+
+    def receive(blocking=true)
+      begin
+        @partial_read ||= ''
+        while @partial_read.size < 4
+          @partial_read += @socket.recv_nonblock(4 - @partial_read.size)
+        end
+        while @partial_read.size < (total_size = 4 + @partial_read.unpack('N')[0])
+          @partial_read += @socket.recv_nonblock(total_size - @partial_read.size)
+        end
+      rescue IO::WaitReadable
+        if blocking
+          IO.select([@socket])
+          retry
+        else
+          return nil
+        end
+      end
+
+      ciphertext,@partial_read = @partial_read.slice(4,@partial_read.size-4),nil
+
+      plaintext = @decryptor.update(ciphertext) + @decryptor.final
+      @decryptor.reset
+
+      return plaintext
+    end
+
+    def close
+      @socket.close
+    end
   end
 end
